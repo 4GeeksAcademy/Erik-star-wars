fb525a319aa18c3e52fb8c8b9f346d88bb6ea75e
Erik Martin <erikmartinfernandez@gmail.com>
2024-12-06 10:55:07 +0000

    updated cdn and navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../../img/Logo.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-black">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">hello</span>
 			</Link>
+			<p>
+			<img className="logo" src={Logo} />
+		</p>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary"></button>
 				</Link>
+				
 			</div>
 		</nav>
 	);
