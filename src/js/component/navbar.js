87e2e1485f9cf53edd526b79855c646d7a537f4d
Erik Martin <erikmartinfernandez@gmail.com>
2024-12-06 12:25:46 +0000

    carrousel update

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,22 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../img/Logo.png";
+import { Detail } from "../views/detail";
+import { Home } from "../views/home";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-black">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">hello</span>
-			</Link>
-			<p>
-			<img className="logo" src={Logo} />
-		</p>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary"></button>
-				</Link>
-				
-			</div>
-		</nav>
+		<div>
+		
+    <nav class="navbar">
+        <div class="navbar-left">
+		<Link to={Home} className="btn"><i class="fa-solid fa-house"></i></Link>
+        </div>
+        <div class="navbar-center">
+		<img src={Logo}/>
+        </div>
+        <div class="navbar-right">
+		
+		<div class="dropdown">
+  <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  <i class="fa-regular fa-heart"></i>
+  </button>
+  <ul class="dropdown-menu dropdown-menu-dark">
+    <li><a class="dropdown-item active" href="#">Fav</a></li>
+  </ul>
+</div>
+          
+        
+        </div>
+    </nav>
+    <div class="navbar-links">
+        <Link to={Detail}><i class="fa-solid fa-location-dot"></i></Link>
+        <a href="#">Video</a>
+        <a href="#">Films</a>
+        <a href="#">Series</a>
+        <a href="#">Games + Interactive</a>
+        <a href="#">Databank</a>
+        <a href="#">Disney+</a>
+    </div>
+	</div>
 	);
-};
+}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,11 +2,27 @@ import React from "react";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="text-center mt-5 home">
+		<h1>Characters</h1>
+		<div class="carousel-container">
+			<div class="carousel">
+				<div class="card">
+					<img src="image1.jpg" alt="Image 1" />
+					<div class="caption">Time to do something... exciting!</div>
+				</div>
+				<div class="card">
+					<img src="image1.jpg" alt="Image 1" />
+					<div class="caption">Time to do something... exciting!</div>
+				</div>
+				<div class="card">
+					<img src="image1.jpg" alt="Image 1" />
+					<div class="caption">Time to do something... exciting!</div>
+				</div>
+				<div class="card">
+					<img src="image1.jpg" alt="Image 1" />
+					<div class="caption">Time to do something... exciting!</div>
+				</div>
+			</div>
+		</div>
 	</div>
 );
