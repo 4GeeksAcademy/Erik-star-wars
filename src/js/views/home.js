243a565253d7465783711768186b472d161c6c9c
Erik Martin <erikmartinfernandez@gmail.com>
2024-12-06 19:39:16 +0000

    card update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,34 +4,59 @@ import { Link } from "react-router-dom";
 import { Detail } from "./detail";
 
 export const Home = () => (
-	<div className="text-center mt-5 home">
-		<div>
-			<h1>Characters</h1>
-			<div class="carousel-container">
+	<div className="mt-5 home">
+		<div className="">
+			<h1 className="text-center">Characters</h1>
+			<div class="carousel-container mx-auto">
 				<div class="carousel">
 
 					<div class="card">
 						<img src="image1.jpg" alt="Image 1"/>
-						<div class="caption">Time to do something... exciting!</div>
-						<div><Link to="/detail" className="btn mb-2">Learn more!</Link></div>
+						<div class="caption d-block mx-2">
+							<div className="m-0">
+								<p>Gender:</p>
+							</div>
+							<div>
+								<p>Hair color:</p>
+							</div>
+							<div>
+								<p>Eye color:</p>
+							</div>
+						</div>
+						<div className="d-flex justify-content-between m-3"><Link to="/detail" className="btn">Learn more!</Link><button className="btn"><i class="fa-regular fa-heart"></i></button></div>
 					</div>
 					<div class="card">
-						<img src="image1.jpg" alt="Image 1" />
+						<img src="image1.jpg" alt="Image 1"/>
 						<div class="caption">Time to do something... exciting!</div>
-						<div><Link to="/detail" className="btn mb-2">Learn more!</Link></div>
+						<div className="d-flex justify-content-between m-3"><Link to="/detail" className="btn">Learn more!</Link><button className="btn"><i class="fa-regular fa-heart"></i></button></div>
 					</div>
 					<div class="card">
-						<img src="image1.jpg" alt="Image 1" />
+						<img src="image1.jpg" alt="Image 1"/>
 						<div class="caption">Time to do something... exciting!</div>
-						<div><Link to="/detail" className="btn mb-2">Learn more!</Link></div>
+						<div className="d-flex justify-content-between m-3"><Link to="/detail" className="btn">Learn more!</Link><button className="btn"><i class="fa-regular fa-heart"></i></button></div>
+					</div>
+					<div class="card">
+						<img src="image1.jpg" alt="Image 1"/>
+						<div class="caption">Time to do something... exciting!</div>
+						<div className="d-flex justify-content-between m-3"><Link to="/detail" className="btn">Learn more!</Link><button className="btn"><i class="fa-regular fa-heart"></i></button></div>
+					</div>
+					<div class="card">
+						<img src="image1.jpg" alt="Image 1"/>
+						<div class="caption">Time to do something... exciting!</div>
+						<div className="d-flex justify-content-between m-3"><Link to="/detail" className="btn">Learn more!</Link><button className="btn"><i class="fa-regular fa-heart"></i></button></div>
+					</div>
+					<div class="card">
+						<img src="image1.jpg" alt="Image 1"/>
+						<div class="caption">Time to do something... exciting!</div>
+						<div className="d-flex justify-content-between m-3"><Link to="/detail" className="btn">Learn more!</Link><button className="btn"><i class="fa-regular fa-heart"></i></button></div>
 					</div>
 				</div>
 			</div>
 		</div>
 
-		<div className="space-ships">
-			<h1>Space ships</h1>
-			<div class="carousel-container">
+		<div className="space-ships ">
+			<h1 className="text-center">Space ships</h1>
+			<div class="carousel-container mx-auto">
 				<div class="carousel">
 					<div class="card">
 						<img src="image1.jpg" alt="Image 1" />
